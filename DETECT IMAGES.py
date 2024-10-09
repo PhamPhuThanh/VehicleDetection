9eb0943efce0f9dbd07b589e99141e5afa08790a
PhamPhuThanh <thanhlxag2002@gmail.com>
2024-10-10 00:02:39 +0700

    detect project

---
+++ DETECT IMAGES.py
@@ -0,0 +1,50 @@
+from ultralytics import YOLO
+import cv2
+import math
+import tkinter as tk
+from tkinter import filedialog
+
+# Tạo hộp thoại mở file để chọn ảnh
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.jpeg;*.png")])
+
+# Đọc ảnh từ đường dẫn file
+img = cv2.imread(file_path)
+
+model = YOLO(r"D:\Yolov8-VehicleDetection\train\weights\best.pt")
+
+
+classNames = ['Car', 'Motorcycle', 'Truck', 'Bus', 'Bicycle']
+class_colors = {
+    'Car': (0, 255, 0),         # màu xanh lá
+    'Motorcycle': (0, 0, 255),  # màu đỏ
+    'Truck': (255, 0, 0),       # màu xanh dương
+    'Bus': (0, 255, 255),       # màu vàng
+    'Bicycle': (255, 255, 0)    # màu xanh lam
+}
+
+# Doing detections using YOLOv8
+results = model(img)
+
+# Once we have the results we will loop through them and we will have the bounding boxes for each of the result
+for r in results:
+    boxes = r.boxes
+    for box in boxes:
+        x1, y1, x2, y2 = box.xyxy[0]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        conf = math.ceil((box.conf[0] * 100)) / 100
+        cls = int(box.cls[0])
+        class_name = classNames[cls]
+        label = f'{class_name}{conf}'
+        t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+        c2 = x1 + t_size[0], y1 - t_size[1] - 3
+        color = class_colors[class_name]
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+        cv2.rectangle(img, (x1, y1), c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
